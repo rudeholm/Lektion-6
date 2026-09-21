54912b55b28569637feebb45eae05a8a6268daf3
Grakkam <grakkam@gmail.com>
2026-09-21 21:45:10 +0200

    Uppgift 5 klar

---
+++ 04-Baklänges/Program.cs
@@ -0,0 +1,10 @@
+﻿/* 4. Baklänges (for)
+Skapa en for-snurra som räknar nedåt från 10 till 1. Efter att snurran är klar ska programmet skriva
+ut 'Gott nytt år!'. */
+
+for (int i = 10; i > 0; i--)
+{
+    Console.WriteLine(i);
+}
+
+Console.WriteLine("Gott nytt år!");
---
+++ 05-Namnlistan/Program.cs
@@ -0,0 +1,16 @@
+﻿/* 5. Namnlistan (foreach)
+Skapa en array eller lista med 5 valfria förnamn. Använd en foreach-snurra för att stega igenom
+listan och skriva ut varje namn i konsolen i stora bokstäver (UPPERCASE). */
+
+string[] names = [
+    "Foo",
+    "Bar",
+    "Baz",
+    "Dinkel",
+    "Flärp",
+    ];
+
+foreach (var name in names)
+{
+    Console.WriteLine(name.ToUpper());
+}
