1a4d94aa75634b9f79cb2514b2f32fda0a69b071
Grakkam <grakkam@gmail.com>
2026-09-21 23:16:05 +0200

    Uppgift 6 klar

---
+++ 06-Guld/Program.cs
@@ -0,0 +1,24 @@
+﻿/* 6. Sökandet efter guld (break)
+Skapa en array med strängar som representerar föremål i en ryggsäck (t.ex. 'Rep', 'Fackla', 'Guld',
+'Karta', 'Kompass'). Använd en snurra för att söka efter 'Guld'. Så fort programmet hittar guld ska
+det skriva ut 'Guld hittat!' och omedelbart avbryta snurran med break. */
+
+string[] backpack = [
+    "Rep",
+    "Fackla",
+    "Elddon",
+    "Guld",
+    "Karta",
+    "Kompass",
+    ];
+
+foreach (var item in backpack)
+{
+    Console.WriteLine(item);
+    if (item.Equals("Guld"))
+    {
+        Console.WriteLine("---");
+        Console.WriteLine("Guld hittat!");
+        break;
+    }
+}
