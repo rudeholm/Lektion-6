5deb479ba13395f4c6d81ac11e395a33d27c3747
Grakkam <grakkam@gmail.com>
2026-09-21 21:35:52 +0200

    Uppgift 3 klar

---
+++ 03-Räknaren/Program.cs
@@ -0,0 +1,10 @@
+﻿/* 3. Räknaren (for)
+Skapa en for-snurra som skriver ut alla tal från 1 upp till 20 på samma rad med ett semikolon och
+mellanslag emellan (t.ex. 1; 2; 3; ... 20;). */
+
+for (int i = 1; i <= 20; i++)
+{
+    Console.Write(i < 20 ? $"{i}; " : $"{i};");
+}
+
+Console.WriteLine("\n\n");
