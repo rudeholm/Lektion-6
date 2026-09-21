2ac6239a01d11eee0b4308a55c30b37f18c85518
Grakkam <grakkam@gmail.com>
2026-09-21 23:20:24 +0200

    Uppgift 7 klar

---
+++ 07-HoppaÖver/Program.cs
@@ -0,0 +1,21 @@
+﻿/* 7. Hoppa över dolda filer (continue)
+Skapa en array med filnamn (strängar) där vissa filer börjar med en punkt '.' (vilket symboliserar
+dolda filer, t.ex. '.config', 'index.html', '.git', 'styles.css'). Använd en snurra för att skriva ut
+filnamnen, men använd continue för att hoppa över alla dolda filer så att de inte visas på skärmen. */
+
+string[] files = [
+    ".",
+    "..",
+    ".config",
+    ".git",
+    "index.html",
+    "styles.css",
+    ];
+
+foreach (var filename in files)
+{
+    if (filename.StartsWith('.'))
+        continue;
+
+    Console.WriteLine(filename);
+}
