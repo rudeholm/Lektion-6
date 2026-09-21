63198856c5f495c086a98f387eef69d8c455a184
Grakkam <grakkam@gmail.com>
2026-09-21 21:18:59 +0200

    Add project files.

---
+++ 01-Välkomstsnurran/Program.cs
@@ -0,0 +1,20 @@
+﻿/* 1.Välkomstsnurran(while)
+    Skapa ett program som frågar användaren om de vill fortsätta köra programmet. Så länge
+användaren svarar 'ja' (eller 'J'), ska programmet skriva ut 'Snurrar vidare...' och fråga igen. Om de
+skriver något annat ska snurran avslutas med ett hejdå-meddelande. */
+
+
+while (true)
+{
+    Console.Write("Vill du fortsätta snurra? ");
+    string input = Console.ReadLine().Trim().ToLower();
+    if (input == "ja" || input == "j")
+    {
+        Console.WriteLine("Snurrar vidare...");
+    }
+    else
+    {
+        Console.WriteLine("Hej då!");
+        break;
+    }
+}
