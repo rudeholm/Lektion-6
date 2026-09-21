f431ba8eb16de8bd8bd92cffd4be30790a16a42f
Grakkam <grakkam@gmail.com>
2026-09-21 21:25:40 +0200

    Uppgift 2 klar

---
+++ 02-Inmatningsskyddet/Program.cs
@@ -0,0 +1,14 @@
+﻿/* 2. Inmatningsskyddet (do-while)
+Skapa ett program där användaren måste skriva in ett hemligt lösenord (t.ex. 'hemligt') för att
+komma vidare. Använd en do-while-snurra så att användaren garanterat får skriva in lösenordet
+minst en gång. Snurran upprepas tills rätt lösenord matas in. */
+
+string password = "hemligt";
+string input;
+
+do
+{
+    Console.Write("Lösen? ");
+    input = Console.ReadLine();
+}
+while (input.Equals(password) == false);
